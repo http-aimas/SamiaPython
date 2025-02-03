85944d29dda55610b1381ad2746c00a9ede6f427
http.aimas <hadiasamia002@gmail.com>
2025-02-03 13:14:55 +0300

    just commit

---
+++ Introduction.py
@@ -0,0 +1,7 @@
+#print is a method to display values
+
+
+print("Samia")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,24 @@
+#Variable is a container used to store a value
+
+_number = 10
+firstname = "Samia"
+lastname = "Hadia"
+language = "Python"
+a = 10
+b = 15
+
+
+
+print(_number)
+print(firstname)
+print(lastname)
+print(firstname, lastname)
+print(language, "is a web programming language")
+print(a + b)
+
+# The sum of 10 and 15 is 25
+a = 10
+b = 15
+
+
+print("The sum of", a, "and", b, "is", a + b )
