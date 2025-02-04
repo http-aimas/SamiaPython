fe1ebb93946f90b58b0e175c65febe53ff80f345
http.aimas <hadiasamia002@gmail.com>
2025-02-04 13:14:58 +0300

    just commit

---
+++ datatype.py
@@ -0,0 +1,36 @@
+
+
+number = 45 # This is an Integer
+num = 66.78 # This is a Float
+text = "Hello" # This is a String
+isMammal = True # Boolean
+
+# Data Structures
+cars = [ "Toyota", "Mercedes", "Ford"] # List
+fruits = ( "banana", "apple", "orange") # Tuple
+countries = { "England", "Italy", "Germany"} # Set
+details = {
+    "first_name": "Brian",
+    "course" : "MIT",
+    "age" : 19,
+    "marital_status" : "single"
+
+
+
+}
+
+
+
+
+print(number)
+print(num)
+print(text)
+print(isMammal)
+print(cars)
+print(fruits)
+print(countries)
+print( details["course"])
+
+# Typecasting
+print(float(number))
+print(int(num))
---
+++ string.py
@@ -0,0 +1,25 @@
+
+text = "Hello World"
+language = "PYTHON"
+greeting = "Hello"
+name = "Samia"
+
+print(text)
+print(language)
+
+#Accessing an element in a string
+print(text[0])
+print(text[1])
+
+#Size of a string
+print(len(text))
+
+#Modifying a  string
+print(text.upper())
+print(language.lower())
+
+# String Concatenation - Joining Strings
+print(greeting+name)
+print(greeting + " " + name)
+
+
